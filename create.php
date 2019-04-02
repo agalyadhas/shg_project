81e32ff626024a48b2ffc3e9790454d056f75962
Thyagarajan Shanmugham <citizenofgnu@gmail.com>
2019-04-02 08:02:38 +0530

    field name member_name changed in members.sql is adopted in the code

--- create.php
+++ create.php
@@ -4,6 +4,7 @@ $connection=new mysqli($hostname,$username,$password,$db);
 if ($connection->connect_error)
 	echo("db error happend");
 mysqli_set_charset($connection,"utf8");
+
 //if ((isset($_POST["member_name"])) and (isset($_POST["address"])))
 {
 $member_name= $_POST["member_name"];
@@ -11,7 +12,9 @@ echo empty($member_name);
 $address=$_POST["address"]; 
 if ((empty($member_name)!=1) and (empty($address)!=1))
 {
-$sql="insert into members(name,address) values('".$member_name."','".$address."');";
+
+$sql="insert into members(member_name,address) values('".$member_name."','".$address."');";
+echo $sql;
 $result=$connection->query($sql);
 mysqli_close($connection);
 echo "record saved";
